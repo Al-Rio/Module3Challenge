6cdd350b1cde4f09ff6d369a170e58cceb9561c6
Ryad Abdelgalil <Ryad.Abdelgalil@go.minneapolis.edu>
2026-02-06 23:13:52 +0000

    Week start on Sunday

--- Module3Challenge/Pages/Index.cshtml.cs
+++ Module3Challenge/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ namespace Module3Challenge.Pages
 {
     public class IndexModel : PageModel
     {
-        // Messages shown on the page
+        
         public string HungerMessage { get; set; } = string.Empty;
         public string SoundMessage { get; set; } = string.Empty;
         public string DayMessage { get; set; } = string.Empty;
@@ -22,47 +22,48 @@ namespace Module3Challenge.Pages
             //  If hunger < 5, Tortoise prefers lettuce
             if (hungerLevel >= 8)
             {
-                HungerMessage = "Lion: Roar! I need a big meal!";
+                HungerMessage = "Lion: Roar! I need a big meal !!";
             }
             else if (hungerLevel >= 5)
             {
-                HungerMessage = "Monkey: Ooh ooh! I'll take some bananas.";
+                HungerMessage = "Monkey: Ooh ooh! I'll take some bananas !!";
             }
             else
             {
-                HungerMessage = "Tortoise: Slow and steady! I'll have some lettuce.";
+                HungerMessage = "Tortoise: Slow and steady! I'll have some lettuce !!";
             }
 
             // ternary operator
             // If hungerLevel >= 8, set SoundMessage to "Listen to the Lion: Roar!"
             // Else, set SoundMessage to "Listen to the Monkey: Ooh ooh!"
+
             SoundMessage = hungerLevel >= 8 ? "Listen to the Lion: Roar!" : "Listen to the Monkey: Ooh ooh!";
 
             // switch statement
             // Use dayOfWeek to set DayMessage with a fun zoo event for each day 
-            // 1 = Monday ==> 7 = Sunday
+
             switch (dayOfWeek)
                 {
                     case 1:
-                        DayMessage = "Monday: Bird show !";
+                        DayMessage = "Sunday: Tours all day !";
                     break;
                     case 2:
-                        DayMessage = "Tuesday: Elephant show !";
+                        DayMessage = "Monday: Bird show !";
                     break;
                     case 3:
-                        DayMessage = "Wednesday: crocodile show !";
+                        DayMessage = "Tuesday: Elephant show !";
                     break;
                     case 4:
-                        DayMessage = "Thursday: Penguin feed !";
+                        DayMessage = "Wednesday: crocodile show !";
                     break;
                     case 5:
-                        DayMessage = "Friday: Family games !";
+                        DayMessage = "Thursday: Penguin feed !";
                     break;
                     case 6:
-                        DayMessage = "Saturday: Crafts and fun !";
+                        DayMessage = "Friday: Family games !";
                     break;
                     case 7:
-                        DayMessage = "Sunday: Tours all day !";
+                        DayMessage = "Saturday: Crafts and fun !";
                     break;
                     default:
                         DayMessage = "Enter a number (1 - 7) for the day's event";
